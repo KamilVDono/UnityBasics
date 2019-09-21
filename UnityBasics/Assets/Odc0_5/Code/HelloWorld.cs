24894c314fb694c6f5bc8f60957d35dbc2e0a6a2
KamilVDono <kamilvdono@gmail.com>
2019-09-21 15:12:20 +0200

    Odc 0.5 - Hello World

---
+++ UnityBasics/Assets/Odc0_5/Code/HelloWorld.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Odc0_5
+{
+	public class HelloWorld : MonoBehaviour
+	{
+		[SerializeField] private string _text;
+
+		public string Text
+		{
+			get => _text;
+			private set => _text = value;
+		}
+
+		private void Awake()
+		{
+			Debug.Log( $"Hello World: {Text}" );
+		}
+	}
+}
+
