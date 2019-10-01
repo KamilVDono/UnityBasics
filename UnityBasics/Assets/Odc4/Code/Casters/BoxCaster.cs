22f2c9ddcca698b0d68fd2ff8f71c841acd8bd38
KamilVDono <kamilvdono@gmail.com>
2019-10-01 17:08:22 +0200

    Podstawy Unity #4 - Fizyka#1 czyli raycast i inne funkcje rzucające (SphereCast, Box and Capsule)

---
+++ UnityBasics/Assets/Odc4/Code/Casters/BoxCaster.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using SF = UnityEngine.SerializeField;
+
+namespace Odc4
+{
+	public class BoxCaster : MonoBehaviour, ICaster
+	{
+		[SF] private Vector3 _boxHalfSize = new Vector3(0.5f, 0.5f, 0.5f);
+		[SF] private Camera _camera;
+
+		public bool Cast( Ray ray, out RaycastHit hitInfo, float maxDistance, int layers )
+		{
+			return Physics.BoxCast( ray.origin, _boxHalfSize, ray.direction, out hitInfo, Quaternion.Euler( _camera.transform.forward ), maxDistance, layers );
+		}
+
+		public RaycastHit[] CastAll( Ray ray, float maxDistance, int layers )
+		{
+			return Physics.BoxCastAll( ray.origin, _boxHalfSize, ray.direction, Quaternion.Euler( _camera.transform.forward ), maxDistance, layers );
+		}
+
+		public int CastNonAlloc( Ray ray, RaycastHit[] results, float maxDistance, int layers )
+		{
+			return Physics.BoxCastNonAlloc( ray.origin, _boxHalfSize, ray.direction, results, Quaternion.Euler( _camera.transform.forward ), maxDistance, layers );
+		}
+	}
+}
---
+++ UnityBasics/Assets/Odc4/Code/Casters/RayCaster.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Odc4
+{
+	public class RayCaster : MonoBehaviour, ICaster
+	{
+		public bool Cast( Ray ray, out RaycastHit hitInfo, float maxDistance, int layers )
+		{
+			return Physics.Raycast( ray, out hitInfo, maxDistance, layers );
+		}
+
+		public RaycastHit[] CastAll( Ray ray, float maxDistance, int layers )
+		{
+			return Physics.RaycastAll( ray, maxDistance, layers );
+		}
+
+		public int CastNonAlloc( Ray ray, RaycastHit[] results, float maxDistance, int layers )
+		{
+			return Physics.RaycastNonAlloc( ray, results, maxDistance, layers );
+		}
+	}
+}
